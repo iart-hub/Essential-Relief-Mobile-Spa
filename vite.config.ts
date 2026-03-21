879df71a517a13e67fce947240959ae3667b35e3
iart-hub <lapiserealismo@gmail.com>
2026-03-21 19:27:37 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: "/Essential-Relief-Mobile-Spa/",
+  build: {
+    outDir: 'docs'  // ← ADICIONE ISSO!
+  }
 })
